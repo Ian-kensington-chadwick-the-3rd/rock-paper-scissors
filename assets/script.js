dec7d7bbaa79439d476cb3c8d3ba60a776455559
nba251522 <inquister112345678@gmail.com>
2023-09-27 13:27:44 -0700

    finished code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,33 @@
-var rps =["rock", "paper", "scissor"]
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-var name = prompt("Enter rps:");
+function getComputerChoice() {
+    var rps = ["rock", "paper", "scissors"];
+    var random = Math.floor(Math.random() * rps.length);
+    return rps[random];
+}
 
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        ties++;
+        return "It's a tie!";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        wins++;
+        return `You win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        losses++;
+        return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+var playerName = prompt("Enter rock, paper, scissors:").toLowerCase();
+var computerChoice = getComputerChoice();
+console.log("Computer says:", computerChoice);
 
-var random =Math.floor(math.random()* rps.length)
+var result = playRound(playerName, computerChoice);
+console.log(result);
+console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
